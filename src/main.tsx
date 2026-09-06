f6020ed2a597b05a6139d71be84c9ef96765a15b
dnpuji24-bit <dnpuji24@gmail.com>
2026-09-06 13:15:02 +0845

    Register scoped React service worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/react/sw.js', { scope: '/react/' }).catch(() => {})
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
