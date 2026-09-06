abdb7e6f9c7cdc6d9ee0b83849c43a208163b6bb
dnpuji24-bit <dnpuji24@gmail.com>
2026-09-06 14:22:57 +0845

    Load modern React theme overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './theme.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
