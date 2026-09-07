ad15f557fe54e08f000af346b5215200c3dec477
dnpuji24-bit <dnpuji24@gmail.com>
2026-09-07 12:24:39 +0845

    Render save status popup globally

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import NetworkStatus from './NetworkStatus'
+import SaveStatusPopup from './SaveStatusPopup'
 import './styles.css'
 import './theme.css'
 import './network.css'
 import './field-ui.css'
+import './save-status.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -17,5 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <NetworkStatus />
+    <SaveStatusPopup />
   </React.StrictMode>,
 )
