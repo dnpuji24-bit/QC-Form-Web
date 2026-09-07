a8d246f70a503c672c1e70e378ef817759dd1c19
dnpuji24-bit <dnpuji24@gmail.com>
2026-09-07 16:49:18 +0845

    Add full-name account change request type

--- src/types.ts
+++ src/types.ts
@@ -24,6 +24,7 @@ export interface AccountChangeRequest {
   username: string
   fullName?: string
   requestType?: string
+  newFullName?: string
   newUsername?: string
   passwordRequested?: boolean
   status?: string
